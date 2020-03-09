f7b1a8ece640e087176100212ba04a154842d845
Dmytro Olshanskyi <olshanskyi@gmail.com>
2020-03-09 21:01:51 +0200

    Update install_ubuntu.sh

--- Docker/install_ubuntu.sh
+++ Docker/install_ubuntu.sh
@@ -1,25 +1,25 @@
-#!/bin/bash
-apt-get update
+sudo apt-get update
 
-apt-get install \
-     apt-transport-https \
-     ca-certificates \
-     curl \
-     gnupg2 \
-     software-properties-common
+sudo apt-get -y install \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  gnupg-agent \
+  software-properties-common
+  
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
 
-apt-key fingerprint 0EBFCD88
-
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
+   
+ sudo apt-get update
 
-apt-get update
+sudo apt-get install -y docker-ce=5:18.09.5~3-0~ubuntu-bionic docker-ce-cli=5:18.09.5~3-0~ubuntu-bionic containerd.io
 
-apt-get install docker-ce -y
+sudo usermod -a -G docker cloud_user
 
-#CHECK
 docker run hello-world
