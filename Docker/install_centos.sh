2ff0b8fd8e4267e81832acfa3cde6e8fb0abf5b3
Dmytro Olshanskyi <olshanskyi@gmail.com>
2020-03-23 18:18:37 +0200

    Update install_centos.sh

--- Docker/install_centos.sh
+++ Docker/install_centos.sh
@@ -1,3 +1,4 @@
+#https://access.redhat.com/documentation/en-US/Red_Hat_Enterprise_Linux_OpenStack_Platform/2/html/Getting_Started_Guide/ch02s03.html
 sudo yum install -y device-mapper-persistent-data lvm2
 sudo yum-config-manager \
     --add-repo \
